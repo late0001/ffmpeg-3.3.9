02608c4a78b4789b13d23f262eb9090a377b7fda
jdqiu <1433935693@qq.com>
2019-12-29 15:55:01 +0800

    first commit

---
+++ ffmpeg_cuvid.d
@@ -0,0 +1,19 @@
+ffmpeg_cuvid.o: ffmpeg_cuvid.c libavutil/hwcontext.h libavutil/buffer.h \
+ libavutil/frame.h libavutil/avutil.h libavutil/common.h \
+ libavutil/attributes.h libavutil/macros.h libavutil/version.h \
+ libavutil/avconfig.h libavutil/mem.h libavutil/error.h \
+ libavutil/rational.h libavutil/mathematics.h libavutil/intfloat.h \
+ libavutil/log.h libavutil/pixfmt.h libavutil/dict.h \
+ libavutil/samplefmt.h libavutil/pixdesc.h ffmpeg.h config.h cmdutils.h \
+ libavcodec/avcodec.h libavutil/samplefmt.h libavutil/attributes.h \
+ libavutil/avutil.h libavutil/buffer.h libavutil/cpu.h \
+ libavutil/attributes.h libavutil/channel_layout.h libavutil/dict.h \
+ libavutil/frame.h libavutil/log.h libavutil/pixfmt.h \
+ libavutil/rational.h libavcodec/version.h libavutil/version.h \
+ libavfilter/avfilter.h libavfilter/version.h libavformat/avformat.h \
+ libavcodec/avcodec.h libavformat/avio.h libavutil/common.h \
+ libavformat/version.h libswscale/swscale.h libswscale/version.h \
+ libavformat/avio.h libavutil/avutil.h libavutil/dict.h libavutil/eval.h \
+ libavutil/fifo.h libavutil/pixfmt.h libavutil/rational.h \
+ libavutil/threadmessage.h libswresample/swresample.h \
+ libswresample/version.h
---
+++ ffmpeg_dxva2.d
@@ -0,0 +1,23 @@
+ffmpeg_dxva2.o: ffmpeg_dxva2.c ffmpeg.h config.h cmdutils.h \
+ libavcodec/avcodec.h libavutil/samplefmt.h libavutil/avutil.h \
+ libavutil/common.h libavutil/attributes.h libavutil/macros.h \
+ libavutil/version.h libavutil/avconfig.h libavutil/mem.h \
+ libavutil/error.h libavutil/rational.h libavutil/mathematics.h \
+ libavutil/intfloat.h libavutil/log.h libavutil/pixfmt.h \
+ libavutil/attributes.h libavutil/avutil.h libavutil/buffer.h \
+ libavutil/cpu.h libavutil/channel_layout.h libavutil/dict.h \
+ libavutil/frame.h libavutil/buffer.h libavutil/dict.h \
+ libavutil/samplefmt.h libavutil/log.h libavutil/pixfmt.h \
+ libavutil/rational.h libavcodec/version.h libavutil/version.h \
+ libavfilter/avfilter.h libavfilter/version.h libavformat/avformat.h \
+ libavcodec/avcodec.h libavformat/avio.h libavutil/common.h \
+ libavformat/version.h libswscale/swscale.h libswscale/version.h \
+ libavformat/avio.h libavutil/avutil.h libavutil/dict.h libavutil/eval.h \
+ libavutil/avutil.h libavutil/fifo.h libavutil/attributes.h \
+ libavutil/pixfmt.h libavutil/rational.h libavutil/threadmessage.h \
+ libswresample/swresample.h libswresample/version.h libavcodec/dxva2.h \
+ libavutil/avassert.h libavutil/log.h libavutil/buffer.h \
+ libavutil/frame.h libavutil/imgutils.h libavutil/pixdesc.h \
+ libavutil/pixfmt.h libavutil/version.h libavutil/rational.h \
+ libavutil/hwcontext.h libavutil/buffer.h libavutil/frame.h \
+ libavutil/hwcontext_dxva2.h
